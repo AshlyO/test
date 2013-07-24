59b7268fc725214e8876762edbcac2a414e3c2fe
Ashly <ashlyoshea@fullsail.edu>
2013-07-23 21:50:12 -0400

    test 1

---
+++ something/script.js
@@ -0,0 +1,7 @@
+once upon a time/**
+ * Created with JetBrains WebStorm.
+ * User: ashlyoshea
+ * Date: 7/23/13
+ * Time: 9:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
